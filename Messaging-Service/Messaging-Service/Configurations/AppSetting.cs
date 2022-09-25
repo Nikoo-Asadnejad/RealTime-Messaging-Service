09e866e4e51b42c09a27830d5da22bbfbfb7da91
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-09-25 11:08:12 +0330

    Feat : add Configurator

---
+++ Messaging-Service/Messaging-Service/Configurations/AppSetting.cs
@@ -0,0 +1,40 @@
+namespace Messaging_Service.Configurations;
+public class AppSetting
+{
+  public Microservices Microservices { get; set; }
+  public Logging Logging { get; set; }
+  public Sentry Sentry { get; set; }
+  public string AllowedHosts { get; set; }
+  public ConnectionStrings connectionStrings { get; set; }
+
+
+}
+
+public class Logging
+{
+  public Loglevel LogLevel { get; set; }
+}
+
+public class Sentry
+{
+  public string Dsn { get; set; }
+
+}
+
+public class ConnectionStrings
+{
+  public string SQLServer { get; set; }
+
+}
+public class Loglevel
+{
+  public string Default { get; set; }
+  public string MicrosoftAspNetCore { get; set; }
+}
+
+public class Microservices
+{
+  public string SmsService { get; set; }
+}
+
+
---
+++ Messaging-Service/Messaging-Service/Configurations/Configurator.cs
@@ -0,0 +1,101 @@
+using ErrorHandlingDll.Configurations;
+using GenericRepositoryDll.Configuration;
+using HttpService.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Messaging_Service.Configurations
+{
+  public static class Configurator
+  {
+
+    public static void InjectServices(IServiceCollection services, IConfiguration configuration)
+    {
+
+      services.AddControllers();
+      services.AddEndpointsApiExplorer();
+
+      services.AddSwaggerGen(c =>
+      {
+        var filePath = Path.Combine(AppContext.BaseDirectory, "MessagingService.xml");
+        c.IncludeXmlComments(filePath);
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+          Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+         {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+          } });
+
+      });
+
+      services.Configure<AppSetting>(configuration);
+
+      services.AddAuthentication();
+ 
+
+      //app db context should be relaced
+      var connection = configuration.GetConnectionString("SQLServer");
+      services.AddDbContext<DbContext>(options => options.UseSqlServer(connection));
+      services.AddScoped<DbContext,DbContext>();
+
+      ErrorHandlingDllConfigurator.InjectServices(services, configuration);
+      HttpServiceConfigurator.InjectHttpService(services);
+      GenericRepositoryConfigurator.InjectServices(services);
+
+
+    }
+
+    public static void ConfigPipeLines(WebApplication app)
+    {
+
+      //  ErrorHandlingDllConfigurator.ConfigureAppPipeline(app);
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+      app.MapControllers();
+
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Messaging-Service API's");
+        });
+      }
+
+
+    }
+
+  }
+
+}
